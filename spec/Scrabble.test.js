1880ebfac626a359c035257fee97a1aade04676c
India Rex <face940@gmail.com>
2020-11-12 15:35:49 +0000

    Score for a or f is correctly returned

--- spec/Scrabble.test.js
+++ spec/Scrabble.test.js
@@ -5,4 +5,12 @@ describe('Scrabble', () => {
     const scrabble = new Scrabble('')
     expect(scrabble.score).toBe(0)
   })
+  test('Successfully returns 1 if the string is a', () => {
+    const scrabble = new Scrabble('a')
+    expect(scrabble.score).toBe(1)
+  })
+  test('Successfully returns 4 if the string is f', () => {
+    const scrabble = new Scrabble('f')
+    expect(scrabble.score).toBe(4)
+  })
 })
--- src/Scrabble.js
+++ src/Scrabble.js
@@ -1,5 +1,11 @@
 export default class Scrabble {
-  constructor() {
-    this.score = 0;
+  constructor(play) {
+    if(play === 'a'){
+      this.score = 1;
+    } else if(play === 'f') {
+      this.score = 4;
+    } else {
+      this.score = 0;
+    } 
   }
 }
